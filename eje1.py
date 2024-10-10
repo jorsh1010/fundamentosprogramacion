7af7b59049707381de5db4fba4797807b122d272
Jorge <cornejojorge632@gmail.com>
2024-10-10 09:00:46 -0700

    01 primer ejercicio

---
+++ eje1.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 1
+Escribir un programaque pregunte al usuario su nombre e imprima un saludo
+Entradas:
+     nombre: string
+     salida:
+     saludo:string
+"""
+nombre = input("Escribe tu nombre:")
+edad = input("Escribe tu edad: ")
+saludo ="hola " + nombre + " de " + edad + " años"
+print(saludo)
